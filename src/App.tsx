a6f2e89c6262b07bdb12532abee974a6dd3e58cd
juanfe <lassojuanfe@gmail.com>
2024-11-12 21:16:53 -0500

    Ajusta ruta del custom-sw para subscribir

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ function App() {
         const permission = await Notification.requestPermission();
         if (permission === "granted") {
           // Crea la suscripción
-          const registration = await navigator.serviceWorker.register("/service-worker.js");
+          const registration = await navigator.serviceWorker.register("./custom-sw.js");
           const subscription = await registration.pushManager.subscribe({
             userVisibleOnly: true,
             applicationServerKey: import.meta.env.VITE_VAPID_PUBLIC_KEY,
